33bb58569bed7fc95f57ed9c71bf43b34ab8c0c9
Egor <shundeeves@yandex.ru>
2022-02-12 14:31:44 +0500

    new commit

--- src/main/java/web/Service/CarServiceImpl.java
+++ src/main/java/web/Service/CarServiceImpl.java
@@ -3,6 +3,7 @@ package web.Service;
 import org.springframework.stereotype.Service;
 import web.Model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,14 +15,20 @@ public class CarServiceImpl implements CarService {
             new Car("Audi", "A4", 2019),
             new Car("Audi", "A6", 2018),
             new Car("Mazda", "RX8", 2008),
-            new Car("Subaru", "ImprezaWRX", 2005)
+            new Car("Subaru", "ImprezaWRX", 2005),
+            new Car("Model_Name1", "1", 2008),
+            new Car("Model_Name2", "2", 2005),
+            new Car("Model_Name3", "3", 2008),
+            new Car("Model_Name4", "4", 2005)
     );
 
     public List<Car> getCar(int count) {
-        if (count == 0) {
+        if (count == 0 || count >= 5) {
             return carsList;
-        } else {
+        }
+        if (count > 0) {
             return carsList.stream().limit(count).collect(Collectors.toList());
         }
+        return new ArrayList<>();
     }
 }
